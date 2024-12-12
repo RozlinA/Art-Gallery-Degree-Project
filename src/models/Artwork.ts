c32397bb6620f7f26e79937e115cc3910597dbe7
RozlinA <144224291+RozlinA@users.noreply.github.com>
2024-12-12 11:44:22 +0100

    Define interfaces for data structures

---
+++ src/models/Artwork.ts
@@ -0,0 +1,14 @@
+import { ArtworkImages } from "./ArtworkImages";
+
+export interface Artwork {
+  id: number;
+  created_at: string;
+  title: string;
+  main_image: string;
+  price?: number;
+  category: string;
+  size?: string;
+  technique?: string;
+  is_available: string;
+  artwork_images?: ArtworkImages[];
+}
---
+++ src/models/ArtworkImages.ts
@@ -0,0 +1,7 @@
+export interface ArtworkImages {
+  id: number;
+  artwork_id: number;
+  image_url: string;
+  alt_text?: string;
+  created_at: string;
+}
