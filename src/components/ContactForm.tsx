3c2a42ef240d93901418a37af708b4c3352a27dd
RozlinA <144224291+RozlinA@users.noreply.github.com>
2024-12-17 10:21:22 +0100

    Add feature to send email when form is submitted

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,8 +1,41 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 export const ContactForm = () => {
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) {
+      console.error("Form is not properly referenced.");
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          alert("Meddelandet skickades! Vi återkommer så snart som möjligt.");
+          (e.target as HTMLFormElement).reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <>
       <form
-        action=""
+        ref={form}
+        onSubmit={sendEmail}
         className="border-black flex flex-col gap-4 p-4 md:p-0 w-[33%]"
       >
         <div className="flex justify-between">
