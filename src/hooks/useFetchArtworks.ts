3e7c243b76652740a6a80cb143d59e4da10e92b3
RozlinA <144224291+RozlinA@users.noreply.github.com>
2024-12-19 15:32:27 +0100

    Refactor custom hook to include fetching data for a single artwork

--- src/hooks/useFetchArtworks.ts
+++ src/hooks/useFetchArtworks.ts
@@ -1,35 +1,63 @@
 import { useEffect, useState } from "react";
 import supabase from "../config/supabaseClient";
 import { Artwork } from "../models/Artwork";
+import { useParams } from "react-router-dom";
 
 export const useFetchArtworks = (category?: string) => {
   const [artworks, setArtworks] = useState<Artwork[]>([]);
+  const [artDetails, setArtDetails] = useState<Artwork | null>(null);
   const [fetchError, setFetchError] = useState<string>("");
 
+  const { artId } = useParams();
+
   useEffect(() => {
+    if (!artId && !category) return;
+
     const fetchArtworks = async () => {
-      if (!category) return;
-      setArtworks([]);
       setFetchError("");
+      setArtworks([]);
+      setArtDetails(null);
 
       try {
-        const { data, error } = await supabase
-          .from("artworks")
-          .select()
-          .eq("category", category || null);
+        if (artId) {
+          const { data, error } = await supabase
+            .from("artworks")
+            .select("*, artwork_images(*)")
+            .eq("id", artId);
 
-        if (error) {
-          setFetchError("Kunde inte hämta konstverk");
-        } else {
-          setArtworks(data || []);
+          if (error || !data || data.length === 0) {
+            setFetchError("Kunde inte hämta konstverk");
+            return;
+          }
+
+          setArtDetails(data[0]);
+          return;
         }
-      } catch {
+
+        if (category) {
+          const { data, error } = await supabase
+            .from("artworks")
+            .select()
+            .eq("category", category);
+
+          if (error || !data || data.length === 0) {
+            setFetchError(
+              error
+                ? "Kunde inte hämta konstverk"
+                : "Inga konstverk hittades för kategorin"
+            );
+            return;
+          }
+
+          setArtworks(data);
+        }
+      } catch (err) {
         setFetchError("Ett oväntat fel inträffade");
       }
     };
 
     fetchArtworks();
-  }, [category]);
+  }, [artId, category]);
 
-  return { artworks, fetchError };
+  return { artworks, artDetails, fetchError };
 };
