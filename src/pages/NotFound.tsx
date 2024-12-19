4b768037274796ecc1f5709886359b281037d5b5
RozlinA <144224291+RozlinA@users.noreply.github.com>
2024-12-19 17:11:06 +0100

    Improve 404 page design: Add navigation button and refine layout

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,31 @@
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
   return (
     <>
-      <h1>Hoppsan! Sidan kunde inte hittas</h1>
-      <p>
-        Det verkar som att sidan du letar efter inte längre finns, har flyttats
-        eller aldrig funnits.
-      </p>
+      <section className="w-screen h-screen flex justify-center items-center">
+        <div className="w-[85%] md:w-[40%] flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center gap-3 text-center">
+            <h1 className="text-8xl font-bold">404</h1>
+            <h2 className="text-2xl font-semibold text-gray-600 mb-4">
+              Fel - Sidan kunde inte hittas
+            </h2>
+          </div>
+
+          <div className="flex flex-col justify-center items-center text-center gap-5">
+            <hr className="w-full" />
+            <p className="text-base text-gray-600">
+              Vi kan inte hitta sidan du söker. Kontrollera länken eller gå till
+              startsidan.
+            </p>
+            <Link to={"/"}>
+              <button className="bg-black text-white font-semibold py-2 px-6 hover:bg-[#3c3c3c] rounded-sm">
+                Till Startsidan
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
